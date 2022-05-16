2ae3470391ba3539f14e32d86c3fe20ac4e19b4b
Vanessa Tauchert <60265204+VanessaTauchert@users.noreply.github.com>
2022-05-16 03:46:25 -0300

    Add files via upload

---
+++ src/main/java/com/example/demo/PrincipalController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PrincipalController {
+
+	@GetMapping("/")
+	public String index() {
+		//chame o template index.html da pasta templates
+		return "index";
+	}
+	
+	@GetMapping("/p1")
+	public String Pagina1() {
+		return "pagina1";
+	}
+	
+	@GetMapping("/p2")
+	public String Pagina2() {
+		return "pagina2";
+	}
+	
+
+}
